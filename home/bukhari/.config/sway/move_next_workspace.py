b46b2ca7403dcdc2ab219168a543c867cd4719de
bukhari <a.bukhari@tworks.co.jp>
2020-12-07 04:49:00 +0900

    sway can move focused container to other workspaces with mod+shift+hjkl now

--- home/bukhari/.config/sway/move_next_workspace.py
+++ home/bukhari/.config/sway/move_next_workspace.py
@@ -9,25 +9,49 @@ def next_workspace(num):
         if window.focused:
             prev_window = window
 
-    if abs(num) == 10 and num > 0:
-        ipc.command("focus up")
-    elif abs(num) == 10 and num < 0:
-        ipc.command("focus down")
-    elif abs(num) == 1 and num < 0:
-        ipc.command("focus left")
-    elif abs(num) == 1 and num > 0:
-        ipc.command("focus right")
+    # print(prev_window.ipc_data)
+    setup = "vertical" # or horizontal
+
+    if setup == "vertical":
+        if abs(num) == 10 and num > 0:
+            ipc.command("move up")
+        elif abs(num) == 10 and num < 0:
+            ipc.command("move down")
+        elif abs(num) == 1 and num < 0:
+            ipc.command("move left")
+        elif abs(num) == 1 and num > 0:
+            ipc.command("move right")
+    else:
+        # for horizontal setup
+        if abs(num) == 10 and num > 0:
+            ipc.command("move right")
+        elif abs(num) == 10 and num < 0:
+            ipc.command("move left")
+        elif abs(num) == 1 and num < 0:
+            ipc.command("move down")
+        elif abs(num) == 1 and num > 0:
+            ipc.command("move up")
 
     for window in ipc.get_tree():
         if window.focused:
             next_window = window
 
-    print('prev', prev_window.id, 'next', next_window.id)
-    if prev_window.id == next_window.id and abs(num) == 1:
-        workspace = ipc.get_tree().find_focused().workspace().num
-        next = workspace + num
-        if next > 0 and next <= 20:
-            ipc.command(f"workspace number {workspace + num}")
+
+    # print(next_window.ipc_data)
+    # print(prev_window.rect.x, prev_window.rect.y, prev_window.deco_rect.x, prev_window.deco_rect.y)
+    # print(next_window.rect.x, next_window.rect.y, next_window.deco_rect.x, next_window.deco_rect.y)
+
+    # print('prev', prev_window.id, 'next', next_window.id)
+    if prev_window.rect.x == next_window.rect.x and \
+        prev_window.rect.y == next_window.rect.y and \
+        prev_window.deco_rect.x == next_window.deco_rect.x and \
+        prev_window.deco_rect.y == next_window.deco_rect.y and \
+        abs(num) == 1:
+            workspace = ipc.get_tree().find_focused().workspace().num
+            next = workspace + num
+            if next > 0 and next <= 20:
+                ipc.command(f"move container to workspace number {next}")
+                ipc.command(f"workspace number {next}")
 
     ipc.main_quit()
     sys.exit(0)
