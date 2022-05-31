c7ff390aa313ba248d085f1d5526952506e57373
Bukhari Ahmed <ahmadieb@gmail.com>
2022-05-31 13:37:27 +0300

    updaet treesitter

--- home/bukhari/.config/nvim/lua/treesitter.lua
+++ home/bukhari/.config/nvim/lua/treesitter.lua
@@ -1,7 +1,7 @@
 local configs = require("nvim-treesitter.configs")
 
 configs.setup {
-  ensure_installed = "maintained",
+  ensure_installed = {'python', 'lua'},
   sync_install = false, 
   ignore_install = { "" }, -- List of parsers to ignore installing
 
