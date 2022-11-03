fe35289e73f46020416375cbad28360c51d150ca
Bukhari Ahmed <ahmadieb@gmail.com>
2022-11-03 13:35:24 +0300

    update bufferline looks

--- home/bukhari/.config/nvim/lua/plugins.lua
+++ home/bukhari/.config/nvim/lua/plugins.lua
@@ -89,12 +89,19 @@ return require('packer').startup(function()
         options = {
             mode = "tabs", -- set to "tabs" to only show tabpages instead
             numbers = function(opts)
-              return string.format('%s', opts.raise(opts.ordinal))
+              return string.format('%s', opts.ordinal)
             end,
             separator_style = {"", ""},
             show_close_icon = false,
             show_buffer_close_icons = false,
-            color_icons = false
+            show_tab_indicators = false,
+            color_icons = false,
+            enforce_regular_tabs = false,
+            max_name_length = 30,
+            indicator = {
+              icon = '', -- this should be omitted if indicator style is not 'icon'
+              style = 'none',
+            },
         }
       }) end }
   -- use { 'rafcamlet/tabline-framework.nvim',
@@ -115,12 +122,6 @@ return require('packer').startup(function()
   -- use { 'lukas-reineke/indent-blankline.nvim', 
   --   config = load_setup('indent') }
   -- use 'romgrk/nvim-treesitter-context'
-  use {'ggVGc/vim-fuzzysearch',
-    config = function() 
-      vim.g['fuzzysearch_match_spaces'] = 1 
-      vim.g['fuzzysearch_prompt'] = '/'
-      vim.g['fuzzysearch_ignorecase'] = 1 
-    end }
   if PACKER_BOOTSTRAP then
     require("packer").sync()
   end
