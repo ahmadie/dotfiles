59c2f8a3fd57293e922e4906a58157cb2e7cda5e
Bukhari Ahmed <ahmadieb@gmail.com>
2024-08-28 07:52:08 +0000

    update p10

--- home/bukhari/.p10k.zsh
+++ home/bukhari/.p10k.zsh
@@ -50,7 +50,7 @@
   # Left prompt segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
-    context                   # user@host
+    # context                   # user@host
     dir                       # current directory
     vcs                       # git status
     # command_execution_time  # previous command duration
