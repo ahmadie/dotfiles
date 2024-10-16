ebc52693c9d4c13cbc29a3773831a416c53af600
Bukhari Ahmed <ahmadieb@gmail.com>
2024-10-16 03:28:01 -0400

    update oil

--- home/bukhari/.config/nvim/lua/oil-setup.lua
+++ home/bukhari/.config/nvim/lua/oil-setup.lua
@@ -1,19 +1,27 @@
-require('oil').setup({
-columns = {
-    "icon",
-    "permissions",
-    "size",
-    "mtime",
-  },
-  use_default_keymaps = true,
-  keymaps = {
-    ["?"] = "actions.show_help",
-    ["q"] = "actions.close",
-  },
-  view_options = {
-    show_hidden = true,
-    sort = {
-      { "ctime", "asc" }
-    },
-  },
+require("oil").setup({
+	columns = {
+		"icon",
+		"permissions",
+		"size",
+		"mtime",
+	},
+	use_default_keymaps = true,
+	delete_to_trash = true,
+	keymaps = {
+		["?"] = "actions.show_help",
+		["q"] = "actions.close",
+		["<C-t>"] = {
+			callback = function()
+				require("oil.actions").select_tab.callback()
+				require("oil.actions").close.callback()
+			end,
+			desc = "Open in new tab and close Oil",
+		},
+	},
+	view_options = {
+		show_hidden = true,
+		sort = {
+			{ "ctime", "asc" },
+		},
+	},
 })
